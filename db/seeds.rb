840c7a3004f5dc76741bd55d8512fb4a81ef37ac
soren essen <sorenessen@sorens-MacBook-Pro-2.local>
2021-04-30 16:01:19 -0400

    Add messages table and abstract code with messages partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "sorenessen", password: "password")
+User.create(username: "john", password: "password")
+User.create(username: "paul", password: "password")
+User.create(username: "ringo", password: "password")
+User.create(username: "george", password: "password")
+
