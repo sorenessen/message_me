a7f061a81b7f1d9e404e64acf13105060a6d7146
soren essen <sorenessen@sorens-MacBook-Pro-2.local>
2021-05-01 07:58:43 -0400

    Adding User signup function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,28 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome to the MessageMe app, #{@user.username}! You have successfully signed up."
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
   get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 
   mount ActionCable.server, at: '/cable'
 end
