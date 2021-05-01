1933a6ebd428d1f6043df077adef11d0293ee61e
soren essen <sorenessen@sorens-MacBook-Pro-2.local>
2021-05-01 10:39:29 -0400

    actioncable fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_cable.url = 'wss://message-me-se.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [
-      'https://message-me-se.herokuapp.com/', 'http://message-me-se.herokuapp.com/']
+  config.action_cable.allowed_request_origins = 'https://message-me-se.herokuapp.com/',
+                                                  'http://message-me-se.herokuapp.com/'
 
 
 end
