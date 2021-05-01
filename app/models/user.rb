e9b43cc17b1bea68eae5be5598e200cd62454939
soren essen <sorenessen@sorens-MacBook-Pro-2.local>
2021-05-01 16:57:32 -0400

    expand username character length to 30

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :username, presence: true,
             uniqueness: { case_sensitive: false },
-            length: { minimum: 3, maximum: 15 }
+            length: { minimum: 3, maximum: 30 }
   has_many :messages
   has_secure_password
 end
