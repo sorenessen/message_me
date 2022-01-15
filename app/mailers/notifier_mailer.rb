509c2744f26f20ef1831b01fd05531ff77f07d8a
soren essen <sorenessen@sorens-MacBook-Pro-2.local>
2022-01-15 18:08:42 -0500

    add mailer

---
+++ app/mailers/notifier_mailer.rb
@@ -0,0 +1,10 @@
+class NotifierMailer < ApplicationMailer
+  default from: 'no-reply@example.com',
+          return_path: 'system@example.com'
+
+  def welcome(recipient)
+    @account = recipient
+    mail(to: recipient.email_address_with_name,
+         bcc: ["bcc@example.com", "Order Watcher <watcher@example.com>"])
+  end
+end
