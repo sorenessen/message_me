2fc56395791da3bca19d5395f9e5c70ba768d662
soren essen <sorenessen@sorens-MacBook-Pro-2.local>
2022-01-16 12:13:56 -0500

    blah

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -4,6 +4,7 @@ class ChatroomController < ApplicationController
   def index
     @message = Message.new
     @messages = Message.custom_display
+    PostMailer.post_created.deliver_later
   end
 
 end
