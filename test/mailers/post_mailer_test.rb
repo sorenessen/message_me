de1077fd84c1858a6e4d94cb3c22b1c039e1ffd5
soren essen <sorenessen@sorens-MacBook-Pro-2.local>
2022-04-06 15:39:00 -0400

    fix test

--- test/mailers/post_mailer_test.rb
+++ test/mailers/post_mailer_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class PostMailerTest < ActionMailer::TestCase
   test "post_created" do
     mail = PostMailer.post_created
-    assert_equal "Post created", mail.subject
+    assert_equal "Tits are for free!", mail.subject
     assert_equal ["sorenessen@gmail.com"], mail.to
     assert_equal ["sorenessen@gmail.com"], mail.from
     assert_match "Hi", mail.body.encoded
