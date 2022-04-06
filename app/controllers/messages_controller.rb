020862791719487c42f7cec7a6f6006d92da01e7
soren essen <sorenessen@sorens-MacBook-Pro-2.local>
2022-04-06 13:29:47 -0400

    fix mailer

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,19 +10,6 @@ class MessagesController < ApplicationController
       ActionCable.server.broadcast "chatroom_channel",
                                    mod_message: message_render(message)
       PostMailer.post_created.deliver_now
-      from = SendGrid::Email.new(email: 'sorenessen@gmail.com')
-      to = SendGrid::Email.new(email: 'grutherf@gmail.com')
-      # to = SendGrid::Email.new(email: 'sorenessen@gmail.com')
-      subject = 'New Activity in CatApp'
-      content = SendGrid::Content.new(type: 'text/plain', value: 'Your friends are trying to reach you. Respond here: https://message-me-se.herokuapp.com/')
-      mail = SendGrid::Mail.new(from, subject, to, content)
-
-      sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-      response = sg.client.mail._('send').post(request_body: mail.to_json)
-      puts response.status_code
-      puts response.body
-      puts response.parsed_body
-      puts response.headers
     end
   end
 
